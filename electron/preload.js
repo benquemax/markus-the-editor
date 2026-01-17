32b9f2211868534f6e4bd7a65c4256bab9d6544f
Tom Himanen <tom.himanen@futuhima.ninja>
2026-01-17 18:32:22 +0200

    chore: wipe working tree to start fresh

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile'),
-  saveFile: (args) => ipcRenderer.invoke('dialog:saveFile', args),
-  onFileChanged: (callback) => ipcRenderer.on('file-changed', (_event, value) => callback(value))
-})
